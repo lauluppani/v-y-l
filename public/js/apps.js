791e7d55ec0aaf059cfdbf017775a52103a1ae78
Lau Luppani <lauluppani@gmail.com>
2015-12-10 22:38:04 -0300

    Countdown in the f*cking house.

---
+++ public/js/apps.js
@@ -0,0 +1,20 @@
+var clock;
+
+$(document).ready(function() {
+	var clock;
+
+	clock = $('.your-clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+        	stop: function() {
+        		$('.message').html('The clock has stopped!')
+        	}
+        }
+    });
+		    
+    clock.setTime(4392309);
+    clock.setCountdown(true);
+    clock.start();
+
+});
