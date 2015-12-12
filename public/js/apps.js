2a9936acbd84d62b21605fd3a3f06f284f76b5c4
Lau Luppani <lauluppani@gmail.com>
2015-12-12 16:31:40 -0300

    Cómo llegar > Custom G. Maps in the f*cking house

--- public/js/apps.js
+++ public/js/apps.js
@@ -6,15 +6,24 @@ $(document).ready(function() {
 	clock = $('.your-clock').FlipClock({
         clockFace: 'DailyCounter',
         autoStart: false,
-        callbacks: {
-        	stop: function() {
-        		$('.message').html('The clock has stopped!')
-        	}
-        }
+        // callbacks: {
+        // 	stop: function() {
+        // 		$('.message').html('The clock has stopped!')
+        // 	}
+        // }
     });
 		    
     clock.setTime(4392309);
     clock.setCountdown(true);
     clock.start();
 
+
+    $('.datos').hide();
+
+    $( "#quiero-ayudar" ).click(function() {
+        $(this).prev('.datos').fadeIn(1000);
+        $(this).addClass('pressed');
+        $(this).text('Muchas gracias!').append('<i class="fa fa-thumbs-o-up"></i>');
+        return false;
+    });
 });
