95485a817d434a5085c61d62d021373e0fdcad65
Lau Luppani <lauluppani@gmail.com>
2015-12-11 23:55:12 -0300

    Countdown > Done! :)

--- static/js/apps.js
+++ static/js/apps.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
 	clock = $('.your-clock').FlipClock({
         clockFace: 'DailyCounter',
         autoStart: false,
-        callbacks: {
-        	stop: function() {
-        		$('.message').html('The clock has stopped!')
-        	}
-        }
+        // callbacks: {
+        // 	stop: function() {
+        // 		$('.message').html('The clock has stopped!')
+        // 	}
+        // }
     });
 		    
     clock.setTime(4392309);
