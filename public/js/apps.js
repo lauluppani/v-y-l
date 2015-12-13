b638360da76ad3fe5cf90fdc070ff7b366c6c368
Lau Luppani <lauluppani@gmail.com>
2015-12-13 00:09:49 -0300

    A bunch of improvements.

--- public/js/apps.js
+++ public/js/apps.js
@@ -1,6 +1,8 @@
 var clock;
 
 $(document).ready(function() {
+
+    // Flipclock Countdown
 	var clock;
 
 	clock = $('.your-clock').FlipClock({
@@ -17,13 +19,23 @@ $(document).ready(function() {
     clock.setCountdown(true);
     clock.start();
 
-
+    // Hide & Show CBU Info
     $('.datos').hide();
-
     $( "#quiero-ayudar" ).click(function() {
         $(this).prev('.datos').fadeIn(1000);
         $(this).addClass('pressed');
         $(this).text('Muchas gracias!').append('<i class="fa fa-thumbs-o-up"></i>');
         return false;
     });
+
+    // Magnific Popup
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled:true
+        },
+        // other options
+    });
+
 });
--- static/js/apps.js
+++ static/js/apps.js
@@ -1,6 +1,8 @@
 var clock;
 
 $(document).ready(function() {
+
+    // Flipclock Countdown
 	var clock;
 
 	clock = $('.your-clock').FlipClock({
@@ -17,13 +19,28 @@ $(document).ready(function() {
     clock.setCountdown(true);
     clock.start();
 
-
+    // Hide & Show CBU Info
     $('.datos').hide();
-
     $( "#quiero-ayudar" ).click(function() {
         $(this).prev('.datos').fadeIn(1000);
         $(this).addClass('pressed');
         $(this).text('Muchas gracias!').append('<i class="fa fa-thumbs-o-up"></i>');
         return false;
     });
+
+    // Magnific Popup
+    $('.galeria-wrapper').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled:true
+        },
+        // other options
+    });
+
+    $('.flip-clock-divider.days .flip-clock-label').text('Dias');
+    $('.flip-clock-divider.hours .flip-clock-label').text('Horas');
+    $('.flip-clock-divider.minutes .flip-clock-label').text('Minutos');
+    $('.flip-clock-divider.seconds .flip-clock-label').text('Segundos');
+
 });
