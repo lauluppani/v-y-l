64d9250a7cb2f30171d0020d770cc38da026e770
Lau Luppani <lauluppani@gmail.com>
2015-12-15 09:27:46 -0300

    Trying to solve an issue with the maps.

--- static/js/apps.js
+++ static/js/apps.js
@@ -19,6 +19,12 @@ $(document).ready(function() {
         return false;
     });
 
+    if ($(window).width() < 992) {
+        $('a#opcionB.active').parents().find('#map-escobar').css('left', '0');
+    }
+    else {
+       // alert('More than 960');
+    }
 
 
     $( "img.flowers.left" ).animate({
